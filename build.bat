afab660b4d11eeb2a19abc947f776af88475465b
Basfer <basf-ol@mail.ru>
2025-08-06 09:29:50 +0300

    builds

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
-go build -o gocl.exe main.go
+go build -o builds/win10_amd64/gocl.exe main.go
 
-SET GOOS=linux&&SET GOARCH=amd64&&go build -o gocl main.go
+SET GOOS=linux&&SET GOARCH=amd64&&go build -o builds/linux_amd64/gocl main.go
