34fa0c3575ae3c713cec39a56b7149d0bb29d7e0
Basfer <basf-ol@mail.ru>
2025-08-06 10:02:08 +0300

    v1.0.1

--- build.bat
+++ build.bat
@@ -1,3 +1,5 @@
 go build -o builds/win10_amd64/gocl.exe main.go
 
 SET GOOS=linux&&SET GOARCH=amd64&&go build -o builds/linux_amd64/gocl main.go
+
+SET GOOS=solaris&&SET GOARCH=sparc64&&go build -o builds/solaris_sparc64/gocl main.go
