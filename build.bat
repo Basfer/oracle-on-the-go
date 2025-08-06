900e71ddda2ac09a25128e1ea180db27851f7b0c
Basfer <basf-ol@mail.ru>
2025-08-06 10:12:35 +0300

    no sparc

--- build.bat
+++ build.bat
@@ -2,5 +2,3 @@ go build -o builds/win10_amd64/gocl.exe main.go
 
 SET GOOS=linux&&SET GOARCH=amd64&&go build -o builds/linux_amd64/gocl main.go
 
-SET GOOS=solaris&&SET GOARCH=sparc64&&go build -o builds/solaris_sparc64/gocl main.go
-
